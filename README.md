5fc5b749c030974002c63583d9a43da034852de6
siedd <saidmwacharo8@gmail.com>
2024-03-20 16:17:08 +0300

    final project

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
+Project Overview:
+My personal portfolio website serves as a digital showcase of my professional achievements, skills, and projects. It aims to provide visitors with an overview of my capabilities, experience, and expertise. The primary goal is to present a compelling narrative of my work and capabilities to potential clients, employers, or collaborators.
+
+Features and functionalities:
+There are four sections in my personal portfolio;
+About Me: A section providing an introduction to me, including my background, skills, and interests.
+Skills: This will continue my technical and soft skills.
+Projects: Here you will find some of my personal projects.
+Contact information: information on how you can reach out to me will be found here
+
+Technologies Used:
+My website will use the following languages and technologies;
+    HTML, CSS and javascript for frontend development
+    Flask, python for API and backend development
+    
+future enhancements:     
+    I will continue updating my project
+
+Contact information:
+    Name: Said Mohamed
+    email: saidmwacharo8@gmail.com
+    phone no: 0742707438
 
--- app.py
+++ app.py
@@ -1,1 +1,47 @@
+###integrate HTML with flask
+
+
+
+from flask import Flask, render_template, request
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+
+# Configure Flask-Mail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'saidmwacharo8@gmail.com'
+app.config['MAIL_PASSWORD'] = 'nikupera14'
+app.config['MAIL_DEFAULT_SENDER'] = 'saidmwacharo8@gmail.com'
+
+mail = Mail(app)
+
+@app.route('/')
+def index():
+    return render_template('portfolio.html',)
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+
+        # Validate form inputs
+        if not name or not email:
+            return 'Please fill in all required fields', 400
+
+        # Send email notification
+        send_email_notification(name, email)
+
+        return 'Form submitted successfully!'
+
+def send_email_notification(name, email):
+    msg = Message(subject='New Contact Form Submission',
+                  recipients=['saidmwacharo8@gmail.com'])
+    msg.body = f'Name: {name}\nEmail: {email}'
+    mail.send(msg)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
